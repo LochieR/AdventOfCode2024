5374865f52d6bd0151016cc778c28a4cb013d591
LochieR <lochie.rickerby@gmail.com>
2024-12-01 17:51:03 +0000

    Improve quicksort function.

--- Sources/SharedAoC/Utils.swift
+++ Sources/SharedAoC/Utils.swift
@@ -1,12 +1,28 @@
 import Foundation
 
 public func quicksort<T: Comparable>(_ a: [T]) -> [T] {
-    guard a.count > 1 else { return a }
+    var array = a
+    quicksortInPlace(&array, low: 0, high: array.count - 1)
+    return array
+}
 
-    let pivot = a[a.count / 2]
-    let less = a.filter { $0 < pivot }
-    let equal = a.filter { $0 == pivot }
-    let greater = a.filter { $0 > pivot }
+private func quicksortInPlace<T: Comparable>(_ a: inout [T], low: Int, high: Int) {
+    if low < high {
+        let pivotIndex = partition(&a, low: low, high: high)
+        quicksortInPlace(&a, low: low, high: pivotIndex - 1)
+        quicksortInPlace(&a, low: pivotIndex + 1, high: high)
+    }
+}
 
-    return quicksort(less) + equal + quicksort(greater)
+private func partition<T: Comparable>(_ a: inout [T], low: Int, high: Int) -> Int {
+    let pivot = a[high]
+    var i = low - 1
+    for j in low..<high {
+        if a[j] < pivot {
+            i += 1
+            a.swapAt(i, j)
+        }
+    }
+    a.swapAt(i + 1, high)
+    return i + 1
 }
